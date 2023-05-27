42671d157f57266aa938ffd1c8755a6d4a809aa5
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2023-05-27 12:18:00 -0700

    Changed word input to command line

--- Code/histogram.py
+++ Code/histogram.py
@@ -20,30 +20,17 @@ def histogram(source_text):
 
 # Return the number of unique words in the histogram
 def unique_words(histogram):
-    print(histogram)
     print('The number of unique words in the histogram is:')
     return len(histogram)
 
-def prompt_user():
-    user_word = input("Enter a word to find its frequency: ")
-    if not user_word.isalpha():
-        user_word = input("Enter a valid word to find its frequency: ")
-    return user_word
-
-# Return the frequency of a given word
 def frequency(word, histogram = histogram(source_text)):
     if word not in histogram:
         return "Word not found."
     else:
         return histogram[word]
 
-
-    
-
 # if main
 if __name__ == '__main__':
-    histogram = histogram(source_text)
-    print(unique_words(histogram))
-    word = prompt_user()
-    print(frequency(word, histogram))
+    print(unique_words(histogram(source_text)))
+    print(frequency(sys.argv[1], histogram(source_text)))
     
