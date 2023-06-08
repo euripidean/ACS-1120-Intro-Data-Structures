6e45979508be48e3b7b878aafa93b1dafa7b096e
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2023-06-08 13:04:48 -0700

    Added index

--- Code/app.py
+++ Code/app.py
@@ -1,5 +1,5 @@
 """Main script, uses other modules to generate sentences."""
-from flask import Flask
+from flask import Flask, render_template
 from weighted import weighted_sample
 
 
@@ -11,7 +11,8 @@ app = Flask(__name__)
 def home():
     """Route that returns a web page containing the generated text."""
     sentence = weighted_sample()
-    return f"<p>{sentence}</p>"
+    return render_template("index.html", sentence=sentence)
+    
 
 
 if __name__ == "__main__":
