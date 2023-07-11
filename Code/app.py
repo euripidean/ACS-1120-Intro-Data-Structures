d668d742be023d3438ac76ad52ea1ddcd03da376
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2023-07-11 11:16:18 -0700

    Completed

--- Code/app.py
+++ Code/app.py
@@ -1,7 +1,6 @@
 """Main script, uses other modules to generate sentences."""
 from flask import Flask, render_template
-from weighted import weighted_sample
-
+from markov_chain import Markov
 
 
 app = Flask(__name__)
@@ -10,13 +9,12 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     """Route that returns a web page containing the generated text."""
-    sentence = weighted_sample()
+    sentence = Markov().generate()
     return render_template("index.html", sentence=sentence)
-    
 
 
 if __name__ == "__main__":
     """To run the Flask server, execute `python app.py` in your terminal.
-       To learn more about Flask's DEBUG mode, visit
-       https://flask.palletsprojects.com/en/2.0.x/server/#in-code"""
+    To learn more about Flask's DEBUG mode, visit
+    https://flask.palletsprojects.com/en/2.0.x/server/#in-code"""
     app.run(debug=True)
